9feec913c8478080db22e7dd9b4e4a6493eeb66d
Dario Kriskovic <dario.kriskovic@digihey.com>
2016-12-07 11:44:58 +0100

    remove unnecessery files and add disable for submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { Article } from './article/article.model.ts';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   articles: Article[];
